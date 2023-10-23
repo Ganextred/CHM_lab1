af62d8cbd42c1fc3941614214df2348b5a565702
Rapirys <ilyarotan@gmail.com>
2023-10-24 00:15:26 +0300

    Add visualization

--- cmlab1.py
+++ cmlab1.py
@@ -1,13 +1,15 @@
 from math import log, floor, sin
 
-from mpmath import ln
+import numpy as np
+from matplotlib import pyplot as plt
+
 
 
 def funct(x):
-    return ln(x)*sin(x)/(x*x*x*x +1)
+    return np.log(x)*np.sin(x)/(x*x*x*x +1)
 
 def funct2(x):
-    return ln(1/x)*sin(1/x)/(1/(x*x)+x*x)
+    return np.log(1/x)*np.sin(1/x)/(1/(x*x)+x*x)
 
 
 def middle_rectangles(f, a, b, n):
@@ -37,10 +39,50 @@ def calculate_with_precision(f, a, b, eps):
     return int_2n
 
 
+def visualize_integral(f, a, b, n):
+    # Calculate the width of each rectangle
+    dx = (b - a) / n
+    integral = 0
+
+    x_values = []
+    y_values = []
+
+    for i in range(n):
+        # Calculate the x-coordinate of the middle of the rectangle
+        x = a + (i + 0.5) * dx
+        # Calculate the height of the rectangle using the function f
+        y = f(x)
+        # Add the area of the rectangle to the integral
+        integral += y * dx
+
+        # Store the x and y values for visualization
+        x_values.append(x)
+        y_values.append(y)
+
+    # Convert lists to NumPy arrays for plotting
+    x_values = np.array(x_values)
+    y_values = np.array(y_values)
+
+    # Plot the function and rectangles
+    x = np.linspace(a, b, 400)
+    y = f(x)
+    plt.plot(x, y, label="f(x)")
+    plt.bar(x_values, y_values, width=dx, align="center", alpha=0.5, label="Rectangles")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
+    plt.title("Middle Rectangles Method")
+    plt.show()
+
+    return integral
+
+
 def main():
     #print(middle_rectangles(funct(1,100,)))
-    print("Метод обрізання границі: ", calculate_with_precision(funct, 1, 100, 0.00005))
+    print("Метод обрізання границі: ", middle_rectangles(funct, 1, 100, 35609))
     print("Метод заміни змінної: ", calculate_with_precision(funct2, 0, 1, 0.0001))
+    visualize_integral(funct, 1, 10, 35)
+    visualize_integral(funct2, 0, 1, 12)
 
 
 
